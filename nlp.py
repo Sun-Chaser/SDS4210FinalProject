9debdc2414872a1ebf78fcdd4d16b915b2089f07
Justin Choi <justinchoi6769@gmail.com>
2025-11-21 20:43:29 -0600

    cluster companies using NLP

---
+++ nlp.py
@@ -0,0 +1,35 @@
+import polars as pl 
+from sklearn.mixture import GaussianMixture 
+from sentence_transformers import SentenceTransformer
+
+df = pl.read_csv("full_data_kalman.csv") 
+
+model = SentenceTransformer("all-MiniLM-L6-v2") 
+embeddings = model.encode(df["sicDescription"])  
+
+gm = GaussianMixture(n_components=10, random_state=76).fit(embeddings) 
+cluster = gm.fit_predict(embeddings) 
+
+df = df.with_columns(
+    pred_cluster = cluster 
+)
+
+df_reduced = (
+    df
+    .unpivot(
+        index = [col for col in df.columns if not col.startswith("20")], 
+        on = [col for col in df.columns if col.startswith("20")], 
+        variable_name = "year_quarter", 
+        value_name = "gross_income"
+    )
+    .drop_nulls("gross_income")
+    .with_columns(
+        year = pl.col("year_quarter").str.slice(0,4).cast(pl.Int32), 
+        gross_income = (pl.col("gross_income") / 1e8) 
+    )
+    .filter(pl.col("year").is_between(2021, 2024))
+    .group_by(["year","cik","entityName","loc","sicDescription","pred_cluster"])
+    .agg(pl.col("gross_income").sum().alias("yearly_gross")) 
+)
+
+
